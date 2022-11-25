13b06c210ee0e9ff3be2514b2df021d1a827ed38
Kush Bhavsar <kushbhavsar393@gmail.com>
2022-11-25 14:31:53 +0530

    updated nov 2022

---
+++ slider with js/js/script.js
@@ -0,0 +1,26 @@
+let flag = 0;
+
+function controller(x){
+    flag = flag + x ;
+    slideshow(flag) ;    
+}
+
+slideshow(flag);
+
+function slideshow(num){
+    let slides = document.getElementsByClassName('slide');
+
+    for(let y of slides){
+        y.style.display = "none";
+    }
+
+    if(num == slides.length){
+        num = 0;
+        flag = 0;
+    }
+    if(num < 0){
+        num = slides.length-1;
+        flag = slides.length-1;
+    }
+    slides[num].style.display = "block";
+}
